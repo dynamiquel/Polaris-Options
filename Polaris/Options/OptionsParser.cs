235ea26129831580641b72ffcd95008bf1d7200f
dynamiquel <50085636+dynamiquel@users.noreply.github.com>
2020-04-28 21:04:46 +0100

    Updated to work with Polaris IO 2004

--- Polaris/Options/OptionsParser.cs
+++ Polaris/Options/OptionsParser.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿//  This file is part of Polaris-Options - A user options system for games.
+//  https://github.com/dynamiquel/Polaris-Options
+//  Copyright (c) 2020 dynamiquel and contributors
+
+//  MIT License
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Polaris.IO;
@@ -14,7 +37,7 @@ namespace Polaris.Options
                 if (volume.Value == null) 
                     return;
                 
-                Yaml.Write(fileLocations[volume.Key], volume.Value);
+                Yaml.WriteAsync(fileLocations[volume.Key], volume.Value);
             }
         }
 
@@ -22,12 +45,16 @@ namespace Polaris.Options
         {
             options = new Dictionary<string, dynamic>();
             
-            if (Yaml.TryRead(filePath, out options, true))
+            try
             {
+                options = Task.Run(async() => await Yaml.ReadAsync<Dictionary<string, dynamic>>(filePath)).Result;
                 return true;
             }
-            
-            return false;
+            catch (Exception e)
+            {
+                UnityEngine.Debug.Log($"Could not read YAML at '{filePath}'. Reason:{e}.");
+                return false;
+            }
         }
     }
 }
